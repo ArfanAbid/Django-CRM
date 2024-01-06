ffa6ad6f409ef61c037117cac9489c6ae36d68d3
Arfan Abid <iffionex111@gmail.com>
2024-01-06 18:26:28 +0500

    ~ Configer views , urls and templates

--- core/website/views.py
+++ core/website/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html',{})
+
+def login_user(request):
+    if request.method == 'POST':
+        user_name=request.POST.get('user_name')
+        password=request.POST.get('password')
+        
+        user=authenticate(username=user_name, password=password)
+        if user is None:
+            messages.info(request,"Invalid password")
+            return redirect('/login')
+        else:
+            login(request,user)
+            messages.success(request,"Successfully Login")
+            # return redirect('/Success_page')    
+            return redirect('/')    
+    else:
+        return render(request, 'login.html')
+    
+def register_user(request):
+    return render(request,'register.html')    
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"Successfully Logout")
+    return redirect('/')
+    # return redirect('home')
+
